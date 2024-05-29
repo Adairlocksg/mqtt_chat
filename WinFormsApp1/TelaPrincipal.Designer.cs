4dc483fa0ef6f2d4f4a5f46953e1081a9f890d05
adair.grassi <adair.grassi@useall.com.bt>
2024-05-29 14:20:31 -0300

    Add project files.

---
+++ WinFormsApp1/TelaPrincipal.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WinFormsApp1
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lb_conteudo = new ListBox();
+            tb_mensagem = new TextBox();
+            btn_enviar = new Button();
+            SuspendLayout();
+            // 
+            // lb_conteudo
+            // 
+            lb_conteudo.FormattingEnabled = true;
+            lb_conteudo.ItemHeight = 15;
+            lb_conteudo.Location = new Point(13, 13);
+            lb_conteudo.Name = "lb_conteudo";
+            lb_conteudo.Size = new Size(223, 319);
+            lb_conteudo.TabIndex = 0;
+            // 
+            // tb_mensagem
+            // 
+            tb_mensagem.Location = new Point(13, 339);
+            tb_mensagem.Name = "tb_mensagem";
+            tb_mensagem.Size = new Size(223, 23);
+            tb_mensagem.TabIndex = 1;
+            // 
+            // btn_enviar
+            // 
+            btn_enviar.Location = new Point(12, 369);
+            btn_enviar.Name = "btn_enviar";
+            btn_enviar.Size = new Size(224, 23);
+            btn_enviar.TabIndex = 2;
+            btn_enviar.Text = "Enviar";
+            btn_enviar.UseVisualStyleBackColor = true;
+            btn_enviar.Click += btn_enviar_Click;
+            // 
+            // TelaPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(248, 403);
+            Controls.Add(btn_enviar);
+            Controls.Add(tb_mensagem);
+            Controls.Add(lb_conteudo);
+            Name = "TelaPrincipal";
+            Text = "Chat MQTT";
+            FormClosing += TelaPrincipal_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lb_conteudo;
+        private TextBox tb_mensagem;
+        private Button btn_enviar;
+    }
+}
---
+++ WinFormsApp1/TelaPrincipal.cs
@@ -0,0 +1,136 @@
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace WinFormsApp1
+{
+    public partial class TelaPrincipal : Form
+    {
+        //private MqttClient _mqttClient;
+        private string _nomeAplicacao = "chat_1";
+        private string _nomeAplicacaoResposta = "chat_2";
+        private IMqttClient _mqttClient;
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+
+            ConectarMqtt();
+            //MqttClientConnectResult connAck = await _mqttClient
+            //_mqttClient = new MqttClient("951727d767984add9c434ee6618d4130.s1.eu.hivemq.cloud", 8883, true, MqttSslProtocols.TLSv1_2, null, null);
+
+            //ConectarMqtt();
+        }
+
+        public async void ConectarMqtt()
+        {
+            _mqttClient = new MqttFactory().CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithCredentials("hivemq.webclient.1716980644116", "f40!.HF5n3o&JqP%KuDe")
+                .WithTlsOptions(new MqttClientTlsOptions
+                {
+                    UseTls = true
+                })
+                .WithTcpServer("951727d767984add9c434ee6618d4130.s1.eu.hivemq.cloud", 8883)
+                .Build();
+
+            MqttClientConnectResult connAck = await _mqttClient!.ConnectAsync(options);
+
+            if (!_mqttClient.IsConnected)
+                throw new Exception("Não foi possível conectar ao broker");
+
+            _mqttClient.ApplicationMessageReceivedAsync += async (m) => await MensagensRecebidas(m);
+
+            MqttClientSubscribeResult suback = await _mqttClient.SubscribeAsync($"chat/{_nomeAplicacao}", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
+        }
+
+        private async Task MensagensRecebidas(MqttApplicationMessageReceivedEventArgs m)
+        {
+            var topico = m.ApplicationMessage.Topic;
+            var mensagem = m.ApplicationMessage.ConvertPayloadToString();
+
+            if (string.IsNullOrEmpty(topico) || string.IsNullOrEmpty(mensagem)) return;
+
+            lb_conteudo.Invoke(new Action(() => { lb_conteudo.Items.Add($"**{mensagem}\r\n"); }));
+        }
+
+
+        private async void btn_enviar_Click(object sender, EventArgs e)
+        {
+            var mensagem = tb_mensagem.Text;
+
+            if (string.IsNullOrEmpty(mensagem)) return;
+
+            if (!_mqttClient.IsConnected) ConectarMqtt();
+
+            try
+            {
+                lb_conteudo.Invoke(new Action(() => { lb_conteudo.Items.Add($"{mensagem}\r\n"); }));
+                tb_mensagem.Clear();
+                MqttClientPublishResult puback = await _mqttClient.PublishStringAsync($"chat/{_nomeAplicacaoResposta}", mensagem, MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async void TelaPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            await _mqttClient.DisconnectAsync();
+        }
+
+        //private void MensagensRecebidas(object sender, MqttMsgPublishEventArgs e)
+        //{
+        //    var topico = e.Topic;
+        //    var mensagem = Encoding.UTF8.GetString(e.Message);
+
+        //    if (string.IsNullOrEmpty(topico) || string.IsNullOrEmpty(mensagem)) return;
+
+        //    lb_conteudo.Invoke(new Action(() => { lb_conteudo.Items.Add($"**{mensagem}\r\n"); }));
+        //}
+
+        //private void ConectarMqtt()
+        //{
+        //    if (_mqttClient is null || _mqttClient.IsConnected) return;
+
+        //    _mqttClient.Connect(_nomeAplicacao, "hivemq.webclient.1716980644116", "f40!.HF5n3o&JqP%KuDe");
+
+        //    if (!_mqttClient.IsConnected)
+        //        throw new Exception("Não foi possível conectar ao broker");
+
+        //    _mqttClient.MqttMsgPublishReceived += MensagensRecebidas;
+
+        //    _mqttClient.Subscribe(new string[] { $"chat/{_nomeAplicacao}" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        //}
+
+        //private void btn_enviar_Click(object sender, EventArgs e)
+        //{
+
+
+        //var mensagem = tb_mensagem.Text;
+
+        //if (string.IsNullOrEmpty(mensagem)) return;
+
+        //if (!_mqttClient.IsConnected) ConectarMqtt();
+
+
+        //try
+        //{
+        //    lb_conteudo.Invoke(new Action(() => { lb_conteudo.Items.Add($"{mensagem}\r\n"); }));
+        //    tb_mensagem.Clear();
+        //    _mqttClient.Publish($"chat/{_nomeAplicacaoResposta}", Encoding.UTF8.GetBytes(mensagem), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, false);
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //    throw;
+        //}
+        //}
+
+        //public void TelaPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        //{
+        //    await _mqttClient.Disconnect();
+        //}
+    }
+}
